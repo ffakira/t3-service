088e2bc58e1688cd5ccb847688ed44aeab89f848
ffakira <55924290+ffakira@users.noreply.github.com>
2024-01-18 17:52:49 +0800

    impl: added dynamic display users

--- frontend/src/app/game/match/[id]/page.tsx
+++ frontend/src/app/game/match/[id]/page.tsx
@@ -5,6 +5,19 @@ import { useAuthCtx } from "@/contexts/AuthContext";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
+interface Players {
+  player?: {
+    username: string;
+    id: number;
+    symbol: boolean;
+  };
+  enemy?: {
+    username: string;
+    id: number;
+    symbol: boolean;
+  };
+}
+
 export default function Match({
   params,
 }: {
@@ -16,6 +29,7 @@ export default function Match({
     new Array(9).fill(undefined)
   );
   const { userId } = useAuthCtx();
+  const [players, setPlayers] = useState<Players>({});
 
   const [seconds, setSeconds] = useState<number>(0);
 
@@ -35,33 +49,41 @@ export default function Match({
   };
 
   useEffect(() => {
-    let timer: string | number | NodeJS.Timeout | undefined;
-
-    if (seconds > 0) {
-      timer = setInterval(() => {
-        console.log(seconds);
-        setSeconds((prevState) => prevState + 1);
-      }, 1000);
-    }
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    console.log(userId);
-
     const getGameMatch = async () => {
-      const req = await fetch(`/api/game/match/${params.id}`);
-      const resp = await req.json();
+      const reqMatch = await fetch(`/api/game/match/${params.id}`);
+      const respMatch = await reqMatch.json();
 
-      if (req.status === 200) {
-        console.log(resp.data)
+      if (reqMatch.status === 200) {
+        const { player, enemy } = respMatch.data;
+        const [reqPlayer, reqEnemy] = await Promise.all([
+          fetch(`/api/user/${player.playerId}`),
+          fetch(`/api/user/${enemy.playerId}`),
+        ]);
+
+        const dataPlayer = await reqPlayer.json();
+        const dataEnemy = await reqEnemy.json();
+
+        setPlayers({
+          player: {
+            symbol: player.symbol,
+            id: player.playerId,
+            username: dataPlayer.data.username,
+          },
+          enemy: {
+            symbol: enemy.symbol,
+            id: enemy.playerId,
+            username: dataEnemy.data.username,
+          },
+        });
       }
     };
     getGameMatch();
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
     return () => {
-      clearInterval(timer);
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  }, [seconds]);
+  }, []);
 
   return (
     <>
@@ -73,25 +95,32 @@ export default function Match({
           </div>
           <div className="flex items-center gap-2 bg-teal-900 p-4">
             <Image
-              src="/images/player_x.png"
-              alt="Player X"
+              src={
+                players.player?.symbol
+                  ? "/images/player_o.png"
+                  : "/images/player_x.png"
+              }
+              alt={players.player?.symbol ? "Player O" : "Player X"}
               width={20}
               height={20}
             />
-            <span>photons</span>
+            <span>{players.player?.username}</span>
           </div>
 
           <div className="flex items-center gap-2 bg-teal-900 p-4">
             <Image
-              src="/images/player_o.png"
-              alt="Player O"
+              src={
+                players.enemy?.symbol
+                  ? "/images/player_o.png"
+                  : "/images/player_x.png"
+              }
+              alt={players.enemy?.symbol ? "Player O" : "Player X"}
               width={20}
               height={20}
             />
-            <span>johndoe</span>
+            <span>{players.enemy?.username}</span>
           </div>
         </div>
-        {/* <p>New Match {params.id} </p> */}
         <GameBoard
           playerTurn={playerTurn}
           setPlayerTurn={setPlayerTurn}
